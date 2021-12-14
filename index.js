eef1806ca3ffbfb03f24432b07bdb1b2f4669a65
Kevin Kelley <kkelley622@gmail.com>
2021-12-14 11:49:25 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+const main = document.querySelector('#main');
+
+main.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.setAttribute("id", "victory");
+
+newHeader.innerHTML = "Kevin is the champion"
+
+document.body.append(newHeader);
+
+
+
+
